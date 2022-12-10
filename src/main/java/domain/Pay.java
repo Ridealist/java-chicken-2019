6d88b502b6ad20e2d29392958e062c6311510d8d
Ridealist <cjsn92@gmail.com>
2022-12-10 22:30:56 +0900

    feat(Pay): 주문 내역으로 부터 주문 사항 통계 내는 메서드 구현

--- src/main/java/domain/Pay.java
+++ src/main/java/domain/Pay.java
@@ -1,22 +1,49 @@
 package domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Pay {
     private static final int MENU_DISCOUNT_CRITERIA = 10;
     private static final int DISCOUNT_PRICE = 10_000;
 
     private final Table table;
-    private final PayMethod payMethod;
+    private final List<Order> orders;
 
-    public Pay(Table table, int number) throws IllegalArgumentException {
+    public Pay(Table table) throws IllegalArgumentException {
         this.table = table;
-        this.payMethod = PayMethod.getMethodByNumber(number);
+        this.orders = OrderRepository.getOrdersByTable(table);
     }
 
-    public int getDiscountPrice() {
+    public int getDiscountPrice(int number) {
         int cost = getOriginalTotalCost() - calculateMenuDiscount();
-        return (int) calculatePayDiscount(cost);
+        return (int) calculatePayDiscount(cost, PayMethod.getMethodByNumber(number));
+    }
+
+    private Set<Menu> getUniqueMenus() {
+        return orders.stream()
+                .map(Order::getMenu)
+                .collect(Collectors.toSet());
+    }
+
+    private int countMenusFormOrders(Menu menu) {
+        return orders.stream()
+                .filter(order -> order.getMenu().equals(menu))
+                .map(Order::getMenuAmount)
+                .mapToInt(menuAmount -> menuAmount)
+                .sum();
+    }
+
+    // TODO 리팩터링 하기!
+    public Map<Menu, Integer> aggregateMenus() {
+        Map<Menu, Integer> menusCounts = new HashMap<>();
+        for (Menu menu : getUniqueMenus()) {
+            menusCounts.put(menu, countMenusFormOrders(menu));
+        }
+        return menusCounts;
     }
 
     private int getOriginalTotalCost() {
@@ -32,7 +59,7 @@ public class Pay {
         return (menuCount / MENU_DISCOUNT_CRITERIA) * DISCOUNT_PRICE;
     }
 
-    private double calculatePayDiscount(int cost) {
+    private double calculatePayDiscount(int cost, PayMethod payMethod) {
         return payMethod.getDiscountPrice(payMethod, cost);
     }
 }
