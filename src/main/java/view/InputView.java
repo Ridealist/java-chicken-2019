9c9452892223e85b674d6835a127ebd4a19f5426
Ridealist <cjsn92@gmail.com>
2022-12-10 19:04:57 +0900

    feat(InputView): 주문 등록 기능 관련 메소드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,4 +15,16 @@ public class InputView {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMenuChoice() {
+        System.out.println();
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuAmount() {
+        System.out.println();
+        System.out.println("## 메뉴의 수량을 입력하세요.");
+        return scanner.nextInt();
+    }
 }
