8bbea56d697ef5a777c651d4ea8e65c883ba3974
Ridealist <cjsn92@gmail.com>
2022-12-10 21:34:32 +0900

    feat(PosController): 메뉴 기능별 실행 메소드 로직 관리 클래스 구현

---
+++ src/main/java/controller/PosController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import domain.Menu;
+import domain.MenuRepository;
+import domain.Order;
+import domain.OrderRepository;
+import domain.Pay;
+import domain.Table;
+import domain.TableRepository;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import view.InputView;
+import view.OutputView;
+
+public class PosController {
+
+    private static final List<Table> tables = TableRepository.tables();
+    private static final List<Menu> menus = MenuRepository.menus();
+
+    public static void registerOrder() {
+        OutputView.printTables(tables);
+        Table table = repeat(TableRepository::findMenuByNumber, InputView::inputTableNumber);
+        repeatMenuAmount(table);
+    }
+
+    public static void pay() {
+        OutputView.printTables(tables);
+        Table table = repeat(TableRepository::findMenuByNumber, InputView::inputTableNumber);
+        List<Order> orders = OrderRepository.findOrdersByTable(table);
+        OutputView.printOrderList(orders);
+        OutputView.printPay(table);
+        int payMethod = repeat(Integer::valueOf, InputView::inputPayMethod);
+        OutputView.printFinalCost(new Pay(table, payMethod));
+    }
+
+    private static void repeatMenuAmount(Table table) {
+        while (true) {
+            try {
+                OutputView.printMenus(menus);
+                Menu menu = repeat(MenuRepository::findMenuByNumber, InputView::inputMenuChoice);
+                int menuAmount = repeat(Integer::valueOf, InputView::inputMenuAmount);
+                OrderRepository.save(table, menu, menuAmount);
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private static <T, R> R repeat(Function<T, R> repository, Supplier<T> inputReader) {
+        try {
+            return repository.apply(inputReader.get());
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return repeat(repository, inputReader);
+        }
+    }
+}
