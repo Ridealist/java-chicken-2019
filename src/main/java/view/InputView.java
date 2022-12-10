7afca7c133755e1e8ba413cd4ffc5c01c844ee31
Ridealist <cjsn92@gmail.com>
2022-12-10 19:33:14 +0900

    refactor(InputView): nextstep Console 클래스 적용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,30 +1,34 @@
 package view;
 
-import java.util.Scanner;
+import Validator.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputFunctionNumber() {
+    public static int inputFunctionNumber() throws IllegalArgumentException {
         System.out.println("## 원하는 기능을 선택하세요.");
-        return scanner.nextInt();
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     public static int inputTableNumber() {
         System.out.println();
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     public static int inputMenuChoice() {
         System.out.println();
         System.out.println("## 등록할 메뉴를 선택하세요.");
-        return scanner.nextInt();
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 
     public static int inputMenuAmount() {
         System.out.println();
         System.out.println("## 메뉴의 수량을 입력하세요.");
-        return scanner.nextInt();
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
 }
