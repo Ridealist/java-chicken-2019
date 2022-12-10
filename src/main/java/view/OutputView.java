d96ca84ab9d16bc150a789537ebc574cfe137001
Ridealist <cjsn92@gmail.com>
2022-12-10 18:45:56 +0900

    feat(View): InputView 및 OutputView 프로그램 첫 실행 화면 구성 메소드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,7 +10,17 @@ public class OutputView {
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
 
+    // TODO enum 등을 활용해 효율적 관리 방안
+    public static void printMainMenu() {
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+        System.out.println();
+    }
+
     public static void printTables(final List<Table> tables) {
+        System.out.println();
         System.out.println("## 테이블 목록");
         final int size = tables.size();
         printLine(TOP_LINE, size);
