232833d701b85e95ee6f102982397b244f464ea4
Ridealist <cjsn92@gmail.com>
2022-12-10 19:08:03 +0900

    feat(OrderRepository): 새로운 Order 객체 저장 및 전체 목록 조회 기능 추가

--- src/main/java/domain/OrderRepository.java
+++ src/main/java/domain/OrderRepository.java
@@ -1,15 +1,24 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OrderRepository {
     private static final List<Order> orders = new ArrayList<>();
 
+    public static void save(Table table, Menu menu, int menuAmount) {
+        orders.add(new Order(table, menu, menuAmount));
+    }
+
     public static List<Order> findOrdersByTable(Table table) {
         return orders.stream()
                 .filter(order -> order.getTable().equals(table))
                 .collect(Collectors.toList());
+    }
+
+    public static List<Order> orders() {
+        return Collections.unmodifiableList(orders);
     }
 }
