2512285b442736aba37b485ba39c0c997b748b01
Ridealist <cjsn92@gmail.com>
2022-12-12 10:22:55 +0900

    refactor(Command): 기능 선택 입력에 대한 유효성 검증 책임 추가 및 메서드 구현

--- src/main/java/controller/Command.java
+++ src/main/java/controller/Command.java
@@ -1,16 +1,25 @@
 package controller;
 
-public enum Command {
-    REGISTER(1, "주문등록"),
-    PAY(2, "결제하기"),
-    QUIT(3, "프로그램 종료");
+import java.util.Arrays;
 
-    private int choice;
+public enum Command {
+    REGISTER("1", "주문등록"),
+    PAY("2", "결제하기"),
+    QUIT("3", "프로그램 종료");
+
+    private String choice;
     private String explanation;
 
-    Command(int choice, String explanation) {
+    Command(String choice, String explanation) {
         this.choice = choice;
         this.explanation = explanation;
+    }
+
+    public static Command findCommandByInput(String input) {
+        return Arrays.stream(values())
+                .filter(command -> command.choice.equals(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("1, 2, 3 중에 한 가지 기능만 선택할 수 있습니다."));
     }
 
     @Override
