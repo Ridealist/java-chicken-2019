fcd1f3ba0f49406d98f5610b9cd37d4b8bd30b19
Ridealist <cjsn92@gmail.com>
2022-12-12 17:55:46 +0900

    refactor(PosController): 메서드 분리, 메서드 이름 명료화, 트랜잭션 단위 기준 메서드 구현

--- src/main/java/controller/PosController.java
+++ src/main/java/controller/PosController.java
@@ -2,8 +2,10 @@ package controller;
 
 import domain.Menu;
 import domain.MenuRepository;
+import domain.Order;
 import domain.OrderRepository;
 import domain.Pay;
+import domain.PayMethod;
 import domain.Table;
 import domain.TableRepository;
 import java.util.List;
@@ -18,52 +20,66 @@ public class PosController {
     private static final List<Menu> menus = MenuRepository.menus();
 
     public static void registerOrder() {
-        OutputView.printTables(tables);
-        Table table = repeat(TableRepository::findMenuByNumber, InputView::inputTableNumber);
-        repeatMenuAmount(table);
+        Table table = getTable();
+        Menu menu = getMenu();
+        Order order = makeOrder(table, menu);
+        OrderRepository.add(order);
     }
 
-    public static void pay() {
-        OutputView.printTables(tables);
-        // TODO 주문 내역이 없는 테이블은 계산 불가 처리
-        Pay pay = repeatPayOnTable();
+    public static void payTable() {
+        Pay pay = makePay();
         OutputView.printOrderList(pay);
         OutputView.printPay(pay);
-        int payMethod = repeat(Integer::valueOf, InputView::inputPayMethod);
+        PayMethod payMethod = choosePayMethod();
         OutputView.printFinalCost(pay, payMethod);
         OrderRepository.delete(pay);
     }
 
-    private static Pay repeatPayOnTable() {
+    private static Table getTable() {
         try {
-            Table table = repeat(TableRepository::findMenuByNumber, InputView::inputTableNumber);
+            OutputView.printTables(tables);
+            return TableRepository.findTableByNumber(InputView.inputTableNumber());
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getTable();
+        }
+    }
+
+    private static Menu getMenu() {
+        try {
+            OutputView.printMenus(menus);
+            return MenuRepository.findMenuByNumber(InputView.inputMenuChoice());
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getMenu();
+        }
+    }
+
+    private static Order makeOrder(Table table, Menu menu) {
+        try {
+            return new Order(table, menu, InputView.inputMenuAmount());
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return makeOrder(table, menu);
+        }
+    }
+
+    private static Pay makePay() {
+        try {
+            Table table = getTable();
             return new Pay(table);
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
-            return repeatPayOnTable();
+            return makePay();
         }
     }
 
-    private static void repeatMenuAmount(Table table) {
-        while (true) {
-            try {
-                OutputView.printMenus(menus);
-                Menu menu = repeat(MenuRepository::findMenuByNumber, InputView::inputMenuChoice);
-                int menuAmount = repeat(Integer::valueOf, InputView::inputMenuAmount);
-                OrderRepository.save(table, menu, menuAmount);
-                break;
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e.getMessage());
-            }
-        }
-    }
-
-    private static <T, R> R repeat(Function<T, R> repository, Supplier<T> inputReader) {
+    private static PayMethod choosePayMethod() {
         try {
-            return repository.apply(inputReader.get());
+            return PayMethod.getMethodByNumber(InputView.inputPayMethod());
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
-            return repeat(repository, inputReader);
+            return choosePayMethod();
         }
     }
 }
