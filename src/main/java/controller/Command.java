b8c352d110a5c144db6b58b586e97f21484f4c8b
Ridealist <cjsn92@gmail.com>
2022-12-12 17:54:54 +0900

    refactor(Command): 입력값 검증 로직 내부화를 위한 파라미터 타입 변경

--- src/main/java/controller/Command.java
+++ src/main/java/controller/Command.java
@@ -3,21 +3,21 @@ package controller;
 import java.util.Arrays;
 
 public enum Command {
-    REGISTER("1", "주문등록"),
-    PAY("2", "결제하기"),
-    QUIT("3", "프로그램 종료");
+    REGISTER(1, "주문등록"),
+    PAY(2, "결제하기"),
+    QUIT(3, "프로그램 종료");
 
-    private String choice;
+    private int choice;
     private String explanation;
 
-    Command(String choice, String explanation) {
+    Command(int choice, String explanation) {
         this.choice = choice;
         this.explanation = explanation;
     }
 
-    public static Command findCommandByInput(String input) {
+    public static Command findCommandByNumber(int number) {
         return Arrays.stream(values())
-                .filter(command -> command.choice.equals(input))
+                .filter(command -> command.choice == number)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("1, 2, 3 중에 한 가지 기능만 선택할 수 있습니다."));
     }
