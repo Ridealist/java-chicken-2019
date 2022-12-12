7c2e1c88870fced127d28af2f6ed78906a943333
Ridealist <cjsn92@gmail.com>
2022-12-12 17:49:59 +0900

    refactor(InputView): InputValidator 삭제, 유효성 검증 InputView 내부 메서드 및 도메인 로직화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,5 @@
 package view;
 
-import Validator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.function.Supplier;
 
@@ -9,38 +8,39 @@ public class InputView {
     public static int inputFunctionNumber() throws IllegalArgumentException {
         System.out.println("## 원하는 기능을 선택하세요.");
         String input = breakOneLineAfterRead(Console::readLine);
-        InputValidator.validateMainChoice(input);
-        return Integer.parseInt(input);
+        return validateNumber(input);
     }
 
     public static int inputTableNumber() throws IllegalArgumentException {
         System.out.println("## 테이블을 선택하세요.");
         String input = breakOneLineAfterRead(Console::readLine);
-        int number = InputValidator.validateNumber(input);
-        // TODO DB 단에서 레코드가 있는지 확인하는 것을 InputView에서 진행해도 되는가...?
-        InputValidator.validateTableNumber(number);
-        return number;
+        return validateNumber(input);
     }
 
     public static int inputMenuChoice() throws IllegalArgumentException {
         System.out.println("## 등록할 메뉴를 선택하세요.");
         String input = breakOneLineAfterRead(Console::readLine);
-        int number = InputValidator.validateNumber(input);
-        InputValidator.validateMenuNumber(number);
-        return number;
+        return validateNumber(input);
     }
 
     public static int inputMenuAmount() {
         System.out.println("## 메뉴의 수량을 입력하세요.");
         String input = breakOneLineAfterRead(Console::readLine);
-        return InputValidator.validateNumber(input);
+        return validateNumber(input);
     }
 
     public static int inputPayMethod() {
         System.out.println("## 신용 카드는 1번. 현금은 2번");
         String input = breakOneLineAfterRead(Console::readLine);
-        InputValidator.validatePayMethod(input);
-        return Integer.parseInt(input);
+        return validateNumber(input);
+    }
+
+    private static int validateNumber(String input) {
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다. 올바른 숫자를 입력해주세요.");
+        }
     }
 
     private static <T> T breakOneLineAfterRead(Supplier<T> reader) {
